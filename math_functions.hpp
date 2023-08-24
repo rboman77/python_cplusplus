9f48132606a7941bd5da2e1b3e7915ec9ef07e35
rboman <rboman@penguin>
2023-08-24 11:45:58 -0700

    update

--- math_functions.hpp
+++ math_functions.hpp
@@ -1,20 +1,10 @@
 #include <string>
 
-#include <pybind11/pybind11.h>
 
-namespace py = pybind11;
-
-
-
-class MathFunction {
-  MathFunction(const std::string label);
+class MathFunctions {
+public:
+  MathFunctions(const std::string label);
   int add_integers(int a, int b);
   int divide_floats(float a, float b);
 };
-
-
-PYBIND11_MODULE(math_functions, m) {
-  py::class_<MathFunction>(m, "MathFunction")
-    .def(py::init<const std::string&>());
-}
 
